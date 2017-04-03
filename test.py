09bc97bcdc94530977f5bfc6f5bdc151b69ba0d2
Ace <252690252@qq.com>
2017-04-03 15:25:10 +0800

    wrote a test py file

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+s1=85
+s2=72
+r=(s2-s1)/s1*100
+if r>0:
+    print('小明成绩提升：%.1f%%' %r)
+elif r<0:
+    print('小明成绩下降：%.1f%%' %abs(r))
+else:
+    print('小明成绩没有变化。')
+    pass
